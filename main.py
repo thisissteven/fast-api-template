f36e8ec5a385515a02c88ee6080db6c43364d31d
Steven <ssteven075@gmail.com>
2023-10-30 11:53:26 +0700

    sementara

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import os
 import uvicorn
 import traceback
-import tensorflow as tf
-
-from pydantic import BaseModel
+import numpy as np
 from urllib.request import Request
 from fastapi import FastAPI, Response, UploadFile
 from utils import load_image_into_numpy_array
+import pickle
+from sklearn import datasets, svm, metrics
+from skimage.feature import local_binary_pattern
+from PIL import Image, ImageOps
 
 # Initialize Model
 # If you already put yout model in the same folder as this main.py
 # You can load .h5 model or any model below this line
 
-load_options = tf.saved_model.LoadOptions(
-    experimental_io_device='/job:localhost')
+with open('model.pickle', 'rb') as handle:
+    model = pickle.load(handle)
+
 
 # If you use h5 type uncomment line below
 # model = tf.keras.models.load_model('./my_model.h5')
@@ -21,13 +24,28 @@ load_options = tf.saved_model.LoadOptions(
 # model = tf.saved_model.load("./my_model_folder")
 
 
-app = FastAPI()
+app = FastAPI()     
 
 
 @app.get("/")
 def index():
     return "Hello world from ML endpoint!"
 
+def compute_lbp(arr):
+    """Find LBP of all pixels.
+    Also perform Vectorization/Normalization to get feature vector.
+    """
+    # LBP function params
+    radius = 3
+    n_points = 8 * radius
+    n_bins = n_points + 2
+    lbp = local_binary_pattern(arr, n_points, radius, 'uniform')
+    lbp = lbp.ravel()
+    feature = np.zeros(n_bins)
+    for i in lbp:
+        feature[int(i)] += 1
+    feature /= np.linalg.norm(feature, ord=1)
+    return feature
 
 @app.post("/predict_image")
 def predict_image(uploaded_file: UploadFile, response: Response):
@@ -36,14 +54,26 @@ def predict_image(uploaded_file: UploadFile, response: Response):
             response.status_code = 400
             return "File is Not an Image"
 
-        image = load_image_into_numpy_array(uploaded_file.file.read())
-        print("Image shape:", image.shape)
+        # image = load_image_into_numpy_array(uploaded_file.file.read())
+        img = Image.open(uploaded_file.file)
+        if img.mode != 'L':
+            img = ImageOps.grayscale(img)
+        arr = np.array(img)
+        # image = load_image_from_directory('./pura.jpg')
+        # gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+        # image = np.dot(image[...,:3], [0.299, 0.587, 0.114])
+        # print(image)
+        features = compute_lbp(arr)
+        # print("Image shape:", image.shape)
+        print("Features shape:", features.shape)
+        prediction = model.predict(np.array([features]))
+        print(prediction)
 
         return "Endpoint not implemented"
     except Exception as e:
         traceback.print_exc()
         response.status_code = 500
-        return "Internal Server Error"
+        return f"Internal Server Error: {e}"
 
 
 port = os.environ.get("PORT", 8080)
--- utils.py
+++ utils.py
@@ -2,6 +2,10 @@ import numpy as np
 
 from PIL import Image
 from io import BytesIO
+# import cv2 as cv
 
 def load_image_into_numpy_array(data):
     return np.array(Image.open(BytesIO(data)))
+
+# def load_image_from_directory(dir):
+#     return cv.imread(dir)
