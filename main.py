01585d563cc1f1843418712fc32da30984497ec6
Steven <ssteven075@gmail.com>
2023-11-01 09:36:14 +0700

    add cors

--- main.py
+++ main.py
@@ -26,6 +26,16 @@ with open('model.pickle', 'rb') as handle:
 
 app = FastAPI()     
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def index():
